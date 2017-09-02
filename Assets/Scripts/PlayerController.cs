b60200397228c3991ab6641da41096e19dfdc755
Unknown <lukasz.molka5@gmail.com>
2017-09-02 03:24:16 +0200

    Uploaded project with basic physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpMultiplier = 10.0f;
+
+    public float groundThreshold = 0.1f;
+
+    Rigidbody2D rb;
+
+    float horizontal;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        GetInput();
+        Move();
+	}
+
+    void GetInput ()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    void Move ()
+    {
+        var velocity = rb.velocity;
+        velocity.x = horizontal * moveSpeed;
+
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            velocity.y = jumpMultiplier * (-Physics2D.gravity.y * rb.mass);
+        }
+        rb.velocity = velocity;
+    }
+
+    bool Grounded()
+    {
+        Collider2D [] cols = Physics2D.OverlapCircleAll(transform.position, groundThreshold);
+        if (cols.Length > 1)
+            return true;
+        else
+            return false;
+    }
+
+    public void WarpPlayer(Vector2 target)
+    {
+        transform.position = target;
+    }
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    static Warp warp;
+
+    public float warpOffset = 1.0f;
+    new BoxCollider2D collider;
+
+	void Awake ()
+    {
+        warp = this;
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+    public static void TryTeleport(Transform other)
+    {
+        warp.TryTeleportLocal(other);
+    }
+
+    void TryTeleportLocal(Transform other)
+    {
+        Vector2 dimensions = collider.size / 2;
+        Vector2 warpPosition = other.position;
+
+        if(other.position.x < -dimensions.x)
+        {
+            warpPosition.x = dimensions.x - warpOffset;        
+        }
+        else if(other.position.x > dimensions.x)
+        {
+            warpPosition.x = -dimensions.x + warpOffset;
+        }
+
+        if (other.position.y < -dimensions.y)
+        {
+            warpPosition.y = dimensions.y - warpOffset;
+        }
+        else if (other.position.y > dimensions.y)
+        {
+            warpPosition.y = -dimensions.y + warpOffset;
+        }
+        other.position = warpPosition;
+    }
+}
