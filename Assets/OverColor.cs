a582233e13ecb9e3ac1002bc54bfb93aa6136aec
ixi150 <ixi150@gmail.com>
2017-09-03 06:26:06 +0200

    Overcolor added, krzyztoff overcolored

---
+++ Assets/OverColor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OverColor : MonoBehaviour
+{
+    Gradient gradientAnimation;
+    float timer, duration;
+    new SpriteRenderer renderer;
+    Material material;
+
+    void Awake ()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        material = renderer.material;
+    }
+	
+	void Update ()
+    {
+		if (timer < duration)
+        {
+            timer = Mathf.MoveTowards(timer, duration, Time.deltaTime);
+            material.SetColor("_OverColor", gradientAnimation.Evaluate(timer / duration));
+        }
+	}
+
+    public void Animate(float duration, Gradient gradient)
+    {
+        gradientAnimation = gradient;
+        this.duration = duration;
+        timer = 0;
+    }
+}
